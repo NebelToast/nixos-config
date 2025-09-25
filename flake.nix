57d6a95813539e71001b76b9d3e34d0ada671994
NebelToast <juliussteude@gmai.com>
2025-09-25 18:32:21 +0200

    add theming

--- flake.nix
+++ flake.nix
@@ -3,37 +3,33 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations.UwU = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./configuration.nix
-          ./hardware-configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-            };
-          }
-        ];
-      };
-      formatter.${system} = pkgs.nixfmt-tree;
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.UwU = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+
+            users.julius = import ./home.nix;
+            # ----------------------
+          };
+        }
+      ];
     };
+    formatter.${system} = pkgs.nixfmt-tree;
+  };
 }
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config,lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
 
@@ -12,27 +12,16 @@
     cava
     rofi
     cliphist
+    wl-clipboard
+    (pkgs.writeScriptBin "rofi-clipboard" "cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | wl-copy")
     btop
     hyprpaper
     davinci-resolve
     brightnessctl
-    (python3Packages.buildPythonApplication {
-  pname = "wallchanger";
-  version = "2.7.9";
-  pyproject = true;
-
-  src = ./wallchanger.py;
-  build-system = with pkgs.python3Packages; [ setuptools ];
-
-  # dependencies = with python3Packages; [
-  #   tornado
-  #   python-daemon
-  # ];
-
-  # meta = {
-  #   # ...
-  # };
-})
+        (pkgs.writeScriptBin "wallchanger" ''
+      #!${lib.getExe pkgs.python3}
+      ${builtins.readFile ./wallchanger.py}
+    '')
   ];
 
   dconf.settings = {
@@ -40,6 +29,46 @@
       color-scheme = "prefer-dark";
     };
   };
+services.cliphist.enable = true;
 
+    systemd.user.services.cliphist.Service.ExecStopPost =
+      "${lib.getExe config.services.cliphist.package} wipe";
+
+
+      gtk = {
+        enable = true;
+        gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+        theme.package = pkgs.gnome-themes-extra;
+        iconTheme.package = pkgs.papirus-icon-theme;
+        font = {
+          name = "Open Sans";
+          package = pkgs.open-sans;
+        };
+      };
+      qt = {
+        enable = true;
+        style.package = pkgs.adwaita-qt;
+        platformTheme.name = "adwaita";
+      };
+      home.pointerCursor = {
+        gtk.enable = true;
+        package = pkgs.bibata-cursors;
+        size = 24;
+      };
+    
+
+
+      gtk = {
+        theme.name = "Adwaita-dark";
+        iconTheme.name = "Papirus-Dark";
+      };
+      qt.style.name = "adwaita-dark";
+      home.pointerCursor.name = "Bibata-Original-Classic";
+    
+programs.direnv = {
+  enable = true;
+  nix-direnv.enable = true;
+  silent = true;
+};
   home.stateVersion = "25.05";
 }
