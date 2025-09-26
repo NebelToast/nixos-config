5031ba6e18ac3aef87d1a8ff8d48020779625978
NebelToast <juliussteude@gmai.com>
2025-09-26 16:06:33 +0200

    add terminal flow

--- home.nix
+++ home.nix
@@ -1,27 +1,62 @@
-{ config,lib, pkgs, ... }:
-{
-  # test
-  home.packages = with pkgs; [
+{ config, lib, pkgs, ... }:
 
+# # Use a 'let' block to define custom packages for better organization
+let
+  terminal-flow = pkgs.python3Packages.buildPythonApplication rec {
+    pname = "terminal-flow";
+    version = "unstable";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "kestalkayden";
+      repo = "terminal-flow";
+      # This is the UPDATED commit hash
+      rev = "b5423350531e4b1f57cd7d090581f67b4ec1b861";
+      # This is the NEW sha256 for the updated commit
+      sha256 = "sha256-CkrO2kYWddn1TjxAaL6rTAu+EWNCVe/iwkR977Kl4RI=";
+    };
+
+    format = "pyproject";
+
+    nativeBuildInputs = with pkgs.python3Packages; [
+      hatchling
+      setuptools
+    ];
+
+    propagatedBuildInputs = [ ];
+
+    meta = with pkgs.lib; {
+      description = "A terminal-based workflow tool";
+      homepage = "https://github.com/kestalkayden/terminal-flow";
+      license = licenses.mit;
+    };
+  };
+in
+
+{
+  home.packages = with pkgs; [
     nemo-with-extensions
     kitty
     waybar
     direnv
+    nurl
     rofi
     wallust
     just
     cbonsai
     spotify
     cava
-    rofi
     cliphist
     wl-clipboard
-    (pkgs.writeScriptBin "rofi-clipboard" "cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | wl-copy")
     btop
     hyprpaper
     davinci-resolve
     brightnessctl
-        (pkgs.writeScriptBin "wallchanger" ''
+
+    # Now you just refer to your custom package by name
+    terminal-flow
+
+    (pkgs.writeScriptBin "rofi-clipboard" "cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | wl-copy")
+    (pkgs.writeScriptBin "wallchanger" ''
       #!${lib.getExe pkgs.python3}
       ${builtins.readFile ./wallchanger.py}
     '')
@@ -32,46 +67,51 @@
       color-scheme = "prefer-dark";
     };
   };
-services.cliphist.enable = true;
 
-    systemd.user.services.cliphist.Service.ExecStopPost =
-      "${lib.getExe config.services.cliphist.package} wipe";
+  services.cliphist.enable = true;
 
+  systemd.user.services.cliphist.Service.ExecStopPost =
+    "${lib.getExe config.services.cliphist.package} wipe";
 
-      gtk = {
-        enable = true;
-        gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-        theme.package = pkgs.gnome-themes-extra;
-        iconTheme.package = pkgs.papirus-icon-theme;
-        font = {
-          name = "Open Sans";
-          package = pkgs.open-sans;
-        };
-      };
-      qt = {
-        enable = true;
-        style.package = pkgs.adwaita-qt;
-        platformTheme.name = "adwaita";
-      };
-      home.pointerCursor = {
-        gtk.enable = true;
-        package = pkgs.bibata-cursors;
-        size = 24;
-      };
-    
+  # This is the MERGED and corrected gtk block
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    font = {
+      name = "Open Sans";
+      package = pkgs.open-sans;
+    };
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+  };
 
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt; 
+    };
+  };
 
-      gtk = {
-        theme.name = "Adwaita-dark";
-        iconTheme.name = "Papirus-Dark";
-      };
-      qt.style.name = "adwaita-dark";
-      home.pointerCursor.name = "Bibata-Original-Classic";
-    
-programs.direnv = {
-  enable = true;
-  nix-direnv.enable = true;
-  silent = true;
-};
+  home.pointerCursor = {
+    name = "Bibata-Original-Classic";
+    package = pkgs.bibata-cursors;
+    size = 24;
+    gtk.enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    silent = true;
+  };
+
   home.stateVersion = "25.05";
 }
